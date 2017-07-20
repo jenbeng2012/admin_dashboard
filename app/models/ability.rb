8a91717fc937556b0897fc62945d577dd31ec00f
jocontreras <jen.almarasy@gmail.com>
2017-07-20 18:01:20 +0200

    add cancancan gem for the roles superadmin, supervisor, user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
     user ||= User.new # guest user (not logged in)
-     if user.superadmin_role?
-       can :manage, :all
-     end
-     if user.supervisor_role?
-       can :manage, User
-     end
+      if user.superadmin_role?
+            can :manage, :all
+            can :access, :rails_admin       # only allow admin users to access Rails Admin
+            can :dashboard                  # allow access to dashboard
+      end
+      if user.supervisor_role?
+            can :access, :rails_admin
+            can :manage, User
+      end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
